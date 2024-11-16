816172ea58197302e1e88071e62a5e3eeeb9175c
auro <auro@aurovm.myguest.virtualbox.org>
2024-11-16 16:04:13 +0000

    Second commit

--- solution/solution/robot_controller.py
+++ solution/solution/robot_controller.py
@@ -4,28 +4,121 @@ import rclpy
 from rclpy.node import Node
 from rclpy.signals import SignalHandlerOptions
 from rclpy.executors import ExternalShutdownException
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+
+from geometry_msgs.msg import Twist, Pose
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+from auro_interfaces.msg import StringWithPose, Item, ItemList
+from auro_interfaces.srv import ItemRequest
+
+from tf_transformations import euler_from_quaternion
+import angles
+
+from enum import Enum
+import random
+import math
+
+LINEAR_VELOCITY  = 0.3
+ANGULAR_VELOCITY = 0.5
+
+TURN_LEFT = 1
+TURN_RIGHT = -1
+
+SCAN_THRESHOLD = 0.5
+SCAN_FRONT = 0
+SCAN_LEFT = 1
+SCAN_BACK = 2
+SCAN_RIGHT = 3
+
+class State(Enum):
+    EXPLORING = 0
+    COLLECTING = 1
+    RETURNING = 2
 
 class RobotController(Node):
 
     def __init__(self):
         super().__init__('robot_controller')
 
-        self.declare_parameter('x', 0.0)
-        self.declare_parameter('y', 0.0)
-        self.declare_parameter('yaw', 0.0)
+        self.state = State.EXPLORING
+        self.pose = Pose()
+        self.yaw = 0.0
+        self.previous_yaw = 0.0
+        self.turn_angle = 0.0
+        self.turn_direction = TURN_LEFT
+        self.scan_triggered = [False] * 4
+        self.items = ItemList() # TODO Look into this as a datastructure, we might want a quicker way to find the closest item
 
-        self.initial_x = self.get_parameter('x').get_parameter_value().double_value
-        self.initial_y = self.get_parameter('y').get_parameter_value().double_value
-        self.initial_yaw = self.get_parameter('yaw').get_parameter_value().double_value
+        client_callback_group = MutuallyExclusiveCallbackGroup()
+        timer_callback_group = MutuallyExclusiveCallbackGroup()
+
+        self.pick_up_service = self.create_client(ItemRequest, '/pick_up_item', callback_group=client_callback_group)
+        self.offload_service = self.create_client(ItemRequest, '/offload_item', callback_group=client_callback_group)
+
+        self.item_subscriber = self.create_subscription(
+            ItemList,
+            '/items',
+            self.item_callback,
+            10, callback_group=timer_callback_group
+        )
+
+        self.odom_subscriber = self.create_subscription(
+            Odometry,
+            'odom',
+            self.odom_callback,
+            10, callback_group=timer_callback_group)
+        
+        self.scan_subscriber = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.scan_callback,
+            QoSPresetProfiles.SENSOR_DATA.value, callback_group=timer_callback_group)
+
+        self.cmd_vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.item_info_publisher = self.create_publisher(ItemInfo, 'item_info', 10)
 
         self.timer_period = 0.1 # 100 milliseconds = 10 Hz
         self.timer = self.create_timer(self.timer_period, self.control_loop)
 
+    def item_callback(self, msg):
+        self.get_logger().info(f"x: {msg.x}, y: {msg.y}")
+
+    def odom_callback(self, msg):
+        self.pose = msg.pose.pose
+
+        (roll, pitch, yaw) = euler_from_quaternion([self.pose.orientation.x,
+                                                    self.pose.orientation.y,
+                                                    self.pose.orientation.z,
+                                                    self.pose.orientation.w])
+        
+        self.yaw = yaw
+
+
+    def scan_callback(self, msg):
+        front_ranges = msg.ranges[331:359] + msg.ranges[0:30]
+        left_ranges  = msg.ranges[31:90]
+        back_ranges  = msg.ranges[91:270]
+        right_ranges = msg.ranges[271:330]
+
+        self.scan_triggered[SCAN_FRONT] = min(front_ranges) < SCAN_THRESHOLD 
+        self.scan_triggered[SCAN_LEFT]  = min(left_ranges)  < SCAN_THRESHOLD
+        self.scan_triggered[SCAN_BACK]  = min(back_ranges)  < SCAN_THRESHOLD
+        self.scan_triggered[SCAN_RIGHT] = min(right_ranges) < SCAN_THRESHOLD
+
     def control_loop(self):
-        self.get_logger().info(f"Initial pose - x: {self.initial_x}, y: {self.initial_y}, yaw: {self.initial_yaw}")
+        match self.state:
 
-
+            case State.EXPLORING:
+                self.get_logger().info(f"Exploring")
+            case State.COLLECTING:
+                self.get_logger().info(f"Collecting")
+            case State.RETURNING:
+                self.get_logger().info(f"Returning")
     def destroy_node(self):
+        msg = Twist()
+        self.cmd_vel_publisher.publish(msg)
+        self.get_logger().info(f"Stopping: {msg}")
         super().destroy_node()
 
 
